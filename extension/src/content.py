751bcb0d5d257130da706016ee82691123fbc19e
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2018-12-01 16:30:42 -0500

    create random squares

--- extension/src/content.py
+++ extension/src/content.py
@@ -1,16 +1,42 @@
+def getSiteHeight():
+    return window.innerHeight
 
-def createWaldo():
-    waldo = document.createElement('DIV')
-    waldo.style.background = 'green'
-    waldo.style.width = '50px'
-    waldo.style.height = '50px'
-    waldo.style.position = 'absolute'
-    waldo.style.top = '0'
-    waldo.style.left = '0'
-    waldo.style.zIndex = '9999'
-    return div
+def getSiteWidth():
+    return window.innerWidth
 
-# waldo = createWaldo()
+def foundWaldo():
+    alert('found waldo')
+
+def createPerson(size, isWaldo=False):
+    top = Math.random() * getSiteHeight()
+    left = Math.random() * getSiteWidth()
+    person = document.createElement('DIV')
+    person.style.width = size + 'px'
+    person.style.height = size + 'px'
+    person.style.position = 'absolute'
+    person.style.top = top + 'px'
+    person.style.left = left + 'px'
+    person.style.zIndex = '9999'
+
+    if isWaldo:
+        person.style.background = 'green'
+        person.onclick = def(event):
+            foundWaldo()
+    else:
+        person.style.background = 'red'
+    return person
+
+def populateFakes(amt):
+    fakeContainer = document.createElement('DIV')
+    for i in range(amt):
+        fake = createPerson(10)
+        fakeContainer.appendChild(fake)
+    return fakeContainer
+
+# def createFakes():
 
 
-document.body.appendChild(createWaldo(waldo))
+
+
+document.body.appendChild(createPerson(10, True))
+document.body.appendChild(populateFakes(300))
